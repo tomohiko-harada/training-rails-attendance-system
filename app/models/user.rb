ebac53f977b7b6e72145256a9051f5a568cb9a1c
tomohiko-harada <tomohiko.harada@jmty.jp>
2025-09-05 10:14:55 +0900

    Userモデルとベースのコントローラー作成

---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ApplicationRecord
+  # 小文字に自動変換
+  before_save { self.email = email.downcase }
+
+  #メールのバリデーション
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true,
+                    uniqueness: { case_sensitive: false },
+                    length: { maximum: 105 },
+                    format: { with: VALID_EMAIL_REGEX }
+  
+  # パスワードのバリデーション
+  has_secure_password
+  validates :password, presence: true,
+                      length: { minimum: 6 },
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   resources :tasks
   root to: "tasks#index"
+
+  get "signup", to: "users#new"
+  resources :users, only: [:show, :create]
 end
